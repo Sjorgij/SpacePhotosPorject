6ef2edaac536fc849236ae094af261c7cc1a832c
Sjorgij <sjorgij@gmail.com>
2023-05-08 16:57:17 +0300

    Убран лишний код
    Словарь заменен на переменные

--- nasa_epic.py
+++ nasa_epic.py
@@ -16,12 +16,10 @@ def main():
     response = requests.get(f"{url}/images", params = params)
     response.raise_for_status()
     for image in response.json():
-        image_info = {
-            "url": "https://api.nasa.gov/EPIC/archive/natural",
-            "date": image["date"].split()[0].replace("-", "/"),
-            "name": f"{image['image']}.png"
-        }
-        url = f"{image_info['url']}/{image_info['date']}/png/{image_info['name']}"
+        img_url: "https://api.nasa.gov/EPIC/archive/natural",
+        img_date: image["date"].split()[0].replace("-", "/"),
+        img_name: f"{image['image']}.png"
+        url = f"{img_url}/{img_date}/png/{img_name}"
         url = requests.get(url, params = params).url
         save_to(url, args.path)
 
